adf37d60ed9e1a37f8207f1df3c28b2842626b28
Muhammad Husnain <bitsol44@gmail.com>
2024-06-14 15:06:11 +0500

    Update config.js

--- src/ui/src/app/firebase/config.js
+++ src/ui/src/app/firebase/config.js
@@ -1,14 +1,7 @@
 import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAhmZfkrg9ASMBPGZq7uP9lj4RgaCzJBBU",
-  authDomain: "car-share-a0cd3.firebaseapp.com",
-  projectId: "car-share-a0cd3",
-  storageBucket: "car-share-a0cd3.appspot.com",
-  messagingSenderId: "324130015363",
-  appId: "1:324130015363:web:aaf46e6ca8f78abef033b6",
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
